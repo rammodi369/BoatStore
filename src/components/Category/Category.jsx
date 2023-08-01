9ccd024275af912d34a196d1fd842a8a917b6413
Ram <rammodi0509@gmail.com>
2023-08-01 20:15:55 +0530

    first commit

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,6 +1,14 @@
 import "./Category.scss";
-const Category = () => {
-    return <div>Category</div>;
+import Products from "../Products/Products"
+;const Category = () => {
+    return <div className="category-main-content">
+    <div className="layout">
+        <div className="category-title">
+            Category Title
+        </div>
+        <Products innerPage={true} />
+    </div>
+    </div>;
 };
 
 export default Category;
--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -1,6 +1,21 @@
 import "./Category.scss";
-const Category = () => {
-    return <div>Category</div>;
+import cat1 from "../../../assets/category/cat-1.jpg"
+;const Category = () => {
+    return <div className="shop-by-category">
+        <div className="categories">
+            <div className="category">
+                <img src={cat1} alt=""/>
+            </div>
+            <div className="category">
+                <img src={cat1} alt=""/>
+            </div>
+            <div className="category">
+                <img src={cat1} alt=""/>
+            </div>
+            <div className="category">
+                <img src={cat1} alt=""/>
+            </div>
+        </div>
+        
+    </div>;
 };
-
-export default Category;
